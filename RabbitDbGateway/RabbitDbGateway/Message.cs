9eefecbff785db254038a6444850175b1e7d172f
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-05-14 15:05:56 +0300

    Add files via upload

---
+++ RabbitDbGateway/RabbitDbGateway/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitConsumerCore
+{
+    class Message
+    {
+        public string corr_id { get; set; }
+        public string sp_name { get; set; }
+        public string db_rows { get; set; }
+    }
+}
